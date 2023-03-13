11476514448357387c6d3bbbe947a80f4a120ca8
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-03-13 19:06:37 +0530

    Fixed duplication deletion while deleting the task

--- src/App.js
+++ src/App.js
@@ -4,46 +4,53 @@ import './App.css';
 
 function App() {
     //Declarations
-    const [tasks, setTask] = useState([]);
+    const [todoList, setTask] = useState([]);
     const [newTask, addNewTask] = useState("");
     // Functions
     const handleChange = (events) => {
         addNewTask(events.target.value);
     };
+    // Addd the Task to the todo LIst
     const addTask2List = () => {
-        const taskVar = [
-            ...tasks,
-            newTask
-        ];
-        setTask(taskVar);
+        const taskObj = {
+            id: todoList.length === 0
+                ? 1
+                : todoList[todoList.length - 1].id + 1,
+            taskName: newTask
+        };
+        setTask([...todoList, taskObj]);
     }
-    const deleteTask = (task) => {
-      const newTodo = tasks.filter((oldTask)=>{
-          if(oldTask === task) return false;
-          return true;
-      })
-      setTask(newTodo);
+    console.log(todoList)
+    // Delete the Task
+    const deleteTask = (id) => {
+        const newTodo = todoList.filter((task) => {
+            if (task.id === id) 
+                return false;
+            return true;
+        })
+        setTask(newTodo);
     }
 
     //Render-return
     return (
 
         <div className='App'>
+            {/* Adding Task Here */}
             <div className='AddTaskHere'>
                 <input onChange={handleChange}/>
                 <button onClick={addTask2List}>Add Task</button>
             </div>
+            {/* Displaying Task Here */}
             <div className='DisplayTaskHere'>
                 {
-                    tasks.map((task) => {
+                    todoList.map((task) => {
                         return <div>
-                            <h3>{task}</h3>
-                            <button onClick={() => deleteTask(task)}>X</button>
+                            <h3>{task.taskName}</h3>
+                            <button onClick={() => deleteTask(task.id)}>X</button>
                         </div>
 
                     })
                 }
-
             </div>
 
         </div>
