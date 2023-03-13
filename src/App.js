663a847e45ccdbba4904a447f1473329f9db62ba
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-03-13 18:28:52 +0530

    Added Delete Function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [tasks , setTask] = useState([]);
-  const [newTask , addNewTask] = useState("");
-  const handleChange = (events) => {
+    //Declarations
+    const [tasks, setTask] = useState([]);
+    const [newTask, addNewTask] = useState("");
+    // Functions
+    const handleChange = (events) => {
         addNewTask(events.target.value);
-  };
-  const addTask2List = () =>{
-        const taskVar = [...tasks, newTask];
+    };
+    const addTask2List = () => {
+        const taskVar = [
+            ...tasks,
+            newTask
+        ];
         setTask(taskVar);
-  }
-  return (
-    <div className='App'>
-      <div className='AddTaskHere'>
-      <input onChange={handleChange} />
-      <button onClick={addTask2List} >Add Task</button>
-      </div>
-      <div className='DisplayTaskHere'>
-        {
-          tasks.map((task)=>{
-            return <h3>{task}</h3>
-          })
-        }
+    }
+    const deleteTask = (task) => {
+      const newTodo = tasks.filter((oldTask)=>{
+          if(oldTask === task) return false;
+          return true;
+      })
+      setTask(newTodo);
+    }
 
-      </div>
-      
-    </div>
-  );
+    //Render-return
+    return (
+
+        <div className='App'>
+            <div className='AddTaskHere'>
+                <input onChange={handleChange}/>
+                <button onClick={addTask2List}>Add Task</button>
+            </div>
+            <div className='DisplayTaskHere'>
+                {
+                    tasks.map((task) => {
+                        return <div>
+                            <h3>{task}</h3>
+                            <button onClick={() => deleteTask(task)}>X</button>
+                        </div>
+
+                    })
+                }
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
