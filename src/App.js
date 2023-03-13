848c1e86ba5e4a60a730f56e2bc5af812a9b19de
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-03-13 17:58:57 +0530

    New Project - TodoApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks , setTask] = useState([]);
+  const [newTask , addNewTask] = useState("");
+  const handleChange = (events) => {
+        addNewTask(events.target.value);
+  };
+  const addTask2List = () =>{
+        const taskVar = [...tasks, newTask];
+        setTask(taskVar);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='AddTaskHere'>
+      <input onChange={handleChange} />
+      <button onClick={addTask2List} >Add Task</button>
+      </div>
+      <div className='DisplayTaskHere'>
+        {
+          tasks.map((task)=>{
+            return <h3>{task}</h3>
+          })
+        }
+
+      </div>
+      
     </div>
   );
 }
